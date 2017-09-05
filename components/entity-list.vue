48d4676717950c4a051b39dd7f0a45e98993094a
Jake A Wood <jake.a.wood@gmail.com>
2017-09-04 20:52:28 -0500

    removed placeholder entities

--- components/entity-list.vue
+++ components/entity-list.vue
@@ -12,16 +12,6 @@
 <script>
 import Entity from '~/components/entity.vue'
 export default {
-	mounted () {
-		for(var i = 0; i < 1; i++) {
-			this.$store.commit('entities/create', { hp : 1, name : 'Bob' })
-			this.$store.commit('entities/create', { hp : 10, name : 'Bobby' })
-			this.$store.commit('entities/create', { hp : 100, name : 'Bobby Bob' })
-			this.$store.commit('entities/create', { hp : 1000, name : 'Bobby Bobby' })
-			this.$store.commit('entities/create', { hp : 10000, name : 'Bobby Bobby Bob' })
-			this.$store.commit('entities/create', { hp : 100000, name : 'Bobby Bobby Bobby' })
-		}
-	},
 	components :  {
 		Entity
 	},
